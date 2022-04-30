771f8c8b148e4b032ad520e34455b8fe0963ad75
goraider <alejandro_gosain@hotmail.com>
2022-04-29 23:41:02 -0500

    se configura thunk y el dispatch

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,5 +1,17 @@
 import { types } from '../types/types';
 
+export const startLoginEmailPassword = (email, password) => {
+    return (dispatch) => {
+
+        
+        setTimeout(() => {
+            dispatch( login(123, 'Pepe') );
+        }, 3500);
+
+
+    }
+}
+
 export const login = (uid, displayName) => ({
     type: types.login,
     payload: {
