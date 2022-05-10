5e32797728851371f4bb31cce45b875ab43a1036
goraider <alejandro_gosain@hotmail.com>
2022-05-10 13:12:03 -0500

    se agregan acciones de loading en el boton de iniciar sesion

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,15 +1,26 @@
 import { types } from '../types/types';
+import { startLoading, finishLoading } from '../actions/ui';
 import { firebase, googleAuthProvider } from '../firebase/firebase-config';
 
 export const startLoginEmailPassword = (email, password) => {
     return (dispatch) => {
 
-        
-        setTimeout(() => {
-            dispatch( login(123, 'Pepe') );
-        }, 3500);
+        dispatch( startLoading() );
+        //start
+        firebase.auth().signInWithEmailAndPassword( email, password )
+            .then( ( {user} )  => {
+                dispatch( login( user.uid, user.displayName ) );
+                
+                dispatch( finishLoading() );
+                console.log(user);
+                
+            })
+            .catch( e => {
+                console.log(e);
+                dispatch( finishLoading() );
+            })
 
-
+            
     }
 }
 
--- src/reducers/uiReducer.js
+++ src/reducers/uiReducer.js
@@ -22,7 +22,18 @@ export const uiReducer = ( state = initialstate, action) => {
                 msgError: null
             }
 
-    
+        case types.uiStartLoading:
+            return {
+                ...state,
+                loading: true
+            }
+
+        case types.uiFinishLoading:
+            return {
+                ...state,
+                loading: false
+            }
+        
         default:
             return state;
     }
